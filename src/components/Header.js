5a0a261c105a3eabf61e5e4a3a65db0921f7e288
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-06-13 19:55:03 +0200

    button move and ragacs svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,20 @@ import '../styles/container.css';
 
 const Header = (props) => {
     return (
-        <div  className="header">
-            <div className="container">
-                <h1 className="header__title">{props.title}</h1>
-                {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
-            </div>
+        <div  className="header" >
+                <div className="header_container">
+                    <img className="header_img" src={require('../styles/ragacs.svg')} alt="something"/>
+                    <div style={{flexBasis: '30%'}}>
+                        <h1 className="header__title">{props.title}</h1>
+                        {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+                    </div>
+                </div>
         </div>
     );
 };
 
 Header.defaultProps = {
-    title: 'Dar*link'
+    title: 'Darling'
 };
 
 export default Header;
